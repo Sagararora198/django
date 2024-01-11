74906a888ac905a5224ee959958b056b508215d0
Sagar <sagar.arora198@gmail.com>
2024-01-11 22:57:49 +0530

    serializer

--- littlelemon/littlelemonAPI/serializers.py
+++ littlelemon/littlelemonAPI/serializers.py
@@ -1,7 +1,26 @@
 from rest_framework import serializers
 from .models import MenuItem
 
+
+
+# class MenuItemSerializer(serializers.Serialize):
+#     id = serializers.IntegerField()
+#     title = serializers.CharField(max_length=255)
+#     price = serializers.DecimalField(max_digits=5,decimal_places=2)
+#     inventory = serializers.IntegerField()
+
 class MenuItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = MenuItem
         fields = ['id','title','price','inventory']
+        
+# for changing name and adding fields 
+# from decimal import Decimal
+# class MenuItemSerializer(serializers.ModelSerializer):
+#     stock = serializers.IntegerField(source='inventory')
+#     price_after_tax = serializers.SerializerMethodField(method_name='calculate_tax')
+#     class Meta:
+#         model = MenuItem
+#         fields = ['id','title','price','stock','price_after_tax']
+#     def calculate_tax(self,product:MenuItem):
+#         return product.price * Decimal(1.1)    
