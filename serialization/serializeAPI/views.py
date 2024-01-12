1ef4bad4045c1a5d4f9c0458a8a0f929ea4825f8
Sagar <sagar.arora198@gmail.com>
2024-01-12 12:24:18 +0530

    filtering and searching

--- serialization/serializeAPI/views.py
+++ serialization/serializeAPI/views.py
@@ -12,6 +12,16 @@ def menu_items(request):
     if request.method == 'GET':
         # items = MenuItem.objects.all()
         items = MenuItem.objects.select_related('category').all()
+        category_name = request.query_params.get('category')
+        to_price = request.query_params.get('to_price')
+        search = request.query_params.get('search')
+        
+        if category_name:
+            items = items.filter(category__title=category_name)
+        if to_price:
+            items = items.filter(price__lte=to_price)  #lte less than equal to    
+        if search:
+            items = items.filter(title__contains=search)     #for case insensetive use icontains    
         serialized_items = MenuItemSerializer(items,many=True)   #the many=true signify you are converting list to json data
         return Response(serialized_items.data)
     if request.method == 'POST':
