d96bcddeb13bbe5e3d4b7dbc612db1cde5407aea
Sagar <sagar.arora198@gmail.com>
2024-01-14 13:58:41 +0530

    authorization and authentication

--- serialization/serializeAPI/urls.py
+++ serialization/serializeAPI/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('menu-items/',views.menu_items),
     path('single-item/<int:id>',views.single_item),
     path('secret/',views.secret),
-    path('api-token-auth',obtain_auth_token) ,   #this endpoint only accept post request (add username and password)
+    path('api-token-auth',obtain_auth_token) ,   #this endpoint only accept post request (add username and password) hit the endpoint for tokens
     path('manager/',views.manager_view),
 ]
