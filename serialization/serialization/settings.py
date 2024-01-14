59050148aaa418c343a8e3824aa8e090ba160fa0
Sagar <sagar.arora198@gmail.com>
2024-01-14 14:20:24 +0530

    djoser

--- serialization/serialization/settings.py
+++ serialization/serialization/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
-    'serializeAPI'
+    'djoser',
+    'serializeAPI',
 ]
 
 MIDDLEWARE = [
@@ -127,6 +128,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES':(
-        'rest_framework.authentication.TokenAuthentication'
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     )
 }   
+
+DJOSER = {
+    'USER_ID_FIELD': 'username',
+    
+}
